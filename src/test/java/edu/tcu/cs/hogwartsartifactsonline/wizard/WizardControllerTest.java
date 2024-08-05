6a2432688d1e245e73d35adf393c607e9eb4ce90
Tariq Amarneh <tariqs.naser@gmail.com>
2024-08-05 15:20:53 +0300

    User authentication and authorization done! Close #5

--- src/test/java/edu/tcu/cs/hogwartsartifactsonline/wizard/WizardControllerTest.java
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/wizard/WizardControllerTest.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 class WizardControllerTest {
 
     @Autowired
